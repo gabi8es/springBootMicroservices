104c126d362f5ad999af39557f2b6cb64ba9bc8c
Gabriel Pallares <GLPS@gft.com>
2018-06-07 12:25:27 +0200

    After Step 18 => Internacionalization

--- src/main/java/com/gabi/learnings/springBootMicroservices/SpringBootMicroservicesApplication.java
+++ src/main/java/com/gabi/learnings/springBootMicroservices/SpringBootMicroservicesApplication.java
@@ -2,6 +2,13 @@ package com.gabi.learnings.springBootMicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBootMicroservicesApplication {
@@ -9,4 +16,23 @@ public class SpringBootMicroservicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroservicesApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		//SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+/* NOT NEEDED ANYMORE ADDED THE BASE IN THE CONFIGURATION FILE
+	@Bean
+	ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+
+		return messageSource;
+	}
+*/
+
 }
--- src/main/java/com/gabi/learnings/springBootMicroservices/helloworld/HelloWorldController.java
+++ src/main/java/com/gabi/learnings/springBootMicroservices/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.gabi.learnings.springBootMicroservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //Controller
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
     //GET
     //URI - /hello-world
     //method - "Hello World"
@@ -21,5 +29,16 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldreturnBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+
+    //GET
+    //URI - /hello-world
+    //method - "Hello World"
+    @GetMapping(path = "/hello-world-internazionalized")
+    //public String helloWorldInt(@RequestHeader(name="Accept-Language", required = false) Locale locale){
+    public String helloWorldInt(){
+
+        return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
     }
 }
